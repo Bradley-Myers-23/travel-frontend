0e8356468e482e7ba441fdb4288992ab8fb19a5b
josh <joshuaoufan@yahoo.com>
2023-05-25 18:29:21 -0500

    more changes tehee

--- src/router.js
+++ src/router.js
@@ -24,6 +24,11 @@ const router = createRouter({
       name: "ingredients",
       component: () => import("./views/IngredientList.vue"),
     },
+    {
+      path: "/Trips",
+      name: "Trips",
+      component: () => import("./views/Trips.vue"),
+    },
   ],
 });
 
