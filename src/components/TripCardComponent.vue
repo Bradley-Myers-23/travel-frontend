c266cb242c5bc5bd0526bf8967bab6eddf0f508b
samckinney1 <samuel.mckinney@eagles.oc.edu>
2023-06-10 14:28:25 -0500

    Finished trip page UI

--- src/components/TripCardComponent.vue
+++ src/components/TripCardComponent.vue
@@ -1,14 +1,18 @@
 <script setup>
 import { onMounted, ref, computed } from "vue";
 import { useRouter } from "vue-router";
-import hotelDayServices from "../services/HotelDayServices.js";
-import tripSiteServices from "../services/TripSiteServices.js";
-import tripDayServices from "../services/TripDayServices.js";
+import HotelDayServices from "../services/HotelDayServices.js";
+import TripSiteServices from "../services/TripSiteServices.js";
+import TripDayServices from "../services/TripDayServices.js";
 import UserServices from "../services/UserServices";
 import UserTripServices from "../services/UserTripServices";
 
 const router = useRouter();
 
+const selectedSite = ref({});
+const selectedHotel = ref({});
+const isViewHotel = ref(false);
+const isViewSite = ref(false);
 const showDetails = ref(false);
 const hotelDays = ref([]);
 const tripSites = ref([]);
@@ -25,16 +29,42 @@ const props = defineProps({
 
 onMounted(async () => {
   await getTripSites();
+  await getHotelDays();
   await getTripDays();
   await getUserTrips();
   await getUsers();
   user.value = JSON.parse(localStorage.getItem("user"));
 });
 
+async function getHotelDays() {
+  await HotelDayServices.getHotelDaysForTrip(props.trip.id)
+    .then((response) => {
+      hotelDays.value = response.data;
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
+
 async function getTripSites() {
-  await tripSiteServices.getTripSitesForTrip(props.trip.id)
+  await TripSiteServices.getTripSitesForTrip(props.trip.id)
     .then((response) => {
       tripSites.value = response.data;
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
+
+async function getTripDays() {
+  await TripDayServices.getTripDaysForTripWithData(
+    props.trip.id
+  )
+    .then((response) => {
+      tripDays.value = response.data;
+    })
+    .catch((error) => {
+      console.log(error);
     });
 }
 
@@ -48,15 +78,6 @@ async function getUserTrips() {
     });
 }
 
-async function getTripDays() {
-  await tripDayServices.getTripDaysForTrip(props.trip.id)
-    .then((response) => {
-      tripDays.value = response.data;
-    })  
-    .catch((error) => {
-      console.log(error);
-    });
-}
 async function getUsers() {
   await UserServices.getUser()
     .then((response) => {
@@ -88,12 +109,46 @@ async function addUserToTable(user) {
   const tripId = props.trip.id;
   await addUserToTrip(userId, tripId);
 }
+
+function editDates(date, days) {
+  var result = new Date(date);
+  result.setDate(result.getDate() + days);
+  return result.toISOString().slice(0, 10);;
+}
+
+function formatPhoneNumber(phoneNumber) {
+  // Assuming the phone number is in a standard format like "1234567890"
+  const areaCode = phoneNumber.slice(0, 3);
+  const firstPart = phoneNumber.slice(3, 6);
+  const secondPart = phoneNumber.slice(6);
+  
+  // Format the phone number as "(123) 456-7890"
+  return `(${areaCode}) ${firstPart}-${secondPart}`;
+}
+
+function openHotelDetails(hotel) {
+  selectedHotel.value = hotel.hotel;
+  isViewHotel.value = true;
+}
+
+function closeHotelDetails() {
+  isViewHotel.value = false;
+}
+
+function openSiteDetails(site) {
+  selectedSite.value = site.site;
+  isViewSite.value = true;
+}
+
+function closeSiteDetails() {
+  isViewSite.value = false;
+}
 </script>
 
 <template>
   <v-card
     class="rounded-lg elevation-5 mb-8"
-    @click="showDetails = !showDetails"
+    @click="showDetails = (!isViewHotel && !isViewSite) ? !showDetails : showDetails"
   >
     <v-card-title class="headline">
       <v-row align="center">
@@ -124,7 +179,7 @@ async function addUserToTable(user) {
     </v-card-text>
     <v-expand-transition>
       <v-card-text class="pt-0" v-show="showDetails">
-        <h2>Travellers Per Trip </h2>
+        <h3>Travellers On This Trip </h3>
  
         <v-list>
           <v-list-item
@@ -144,23 +199,128 @@ async function addUserToTable(user) {
 
         <h3>Calendar</h3>
         <br>
-        <v-list class="body-1"
-            v-for="day in tripDays"
-            :key="day.id"
-          >
-            <b>Day {{ day.dayNumber}} </b>
-            <v-list-item
+        <v-table>
+          <tbody>
+            <tr>
+              <td>Day Number</td>
+              <td>Hotel</td>
+              <td>Sites</td>
+            </tr>
+            <tr v-for="day in tripDays" :key="day.id">
+              <td>Day {{ day.dayNumber }}: {{ editDates(trip.startDate, day.dayNumber-1)  }}</td>
+              <td>
+                <v-chip
+                  size="small"
+                  v-for="hotel in day.hotelDay"
+                  :key="hotel.id"
+                  pill
+                  @click="openHotelDetails(hotel)"
+                  >{{ hotel.hotel.name }}</v-chip
+                ><br>
+              </td>
+              <td>
+                <v-chip
+                  size="small"
                   v-for="site in day.tripSite"
                   :key="site.id"
-                  >{{ site.site.name }}</v-list-item
-                >
-          </v-list>
-        <h4> {{UserServices.getUser.value}}</h4>
-
+                  pill
+                  @click="openSiteDetails(site)"
+                  >{{ site.site.name }}</v-chip
+                ><br>
+              </td>
+            </tr>
+          </tbody>
+        </v-table>
       </v-card-text>
       
     </v-expand-transition>
   </v-card>
+
+  <v-dialog
+      persistent
+      :model-value="isViewHotel"
+    >
+      <v-card class="rounded-lg elevation-5">
+        <v-card-title class="headline mb-2">
+          Hotel Details
+        </v-card-title>
+        <v-card-text>
+          <v-table class="rounded-lg elevation-5">
+        <thead>
+          <tr>
+            <th class="text-left">Name</th>
+            <th class="text-left">Phone Number</th>
+            <th class="text-left">Address</th>
+          </tr>
+        </thead>
+          <tbody
+          >
+            <tr>
+              <td>{{ selectedHotel.name }}</td>
+              <td>{{ formatPhoneNumber(selectedHotel.PhoneNumber) }}</td>
+              <td>
+                <div>
+                  <span>{{ selectedHotel.Address1 }}</span>
+                  <br />
+                  <span>{{ selectedHotel.Address2 }}</span>
+                  <br />
+                  <span>{{ selectedHotel.State }}, {{ selectedHotel.City }} , {{ selectedHotel.ZipCode }} </span>
+                </div>
+              </td>
+            </tr>
+          </tbody>
+      </v-table>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn
+            variant="flat"
+            color="secondary"
+            @click="closeHotelDetails()"
+            >Close</v-btn
+          >
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+
+    <v-dialog
+      persistent
+      :model-value="isViewSite"
+    >
+      <v-card class="rounded-lg elevation-5">
+        <v-card-title class="headline mb-2">
+          Site Details
+        </v-card-title>
+        <v-card-text>
+          <v-table class="rounded-lg elevation-5">
+        <thead>
+          <tr>
+            <th class="text-left">Name</th>
+            <th class="text-left">Address</th>
+            <th class="text-left">Description</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>{{ selectedSite.name }}</td>
+            <td>{{ selectedSite.address }}</td>
+            <td>{{ selectedSite.description }}</td>
+          </tr>
+        </tbody>
+      </v-table>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn
+            variant="flat"
+            color="secondary"
+            @click="closeSiteDetails()"
+            >Close</v-btn
+          >
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+
   <v-dialog v-model="addUser">
       
       <v-card>             
